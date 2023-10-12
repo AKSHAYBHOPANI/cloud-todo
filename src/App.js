3d56696fbb8cf50b15eac9031d291ee5e6348676
Akshay Bhopani <akshaybhopani@gmail.com>
2023-10-12 17:30:37 +0530

    * Adding licence headers.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
+// MIT License
+
+// Copyright (c) 2023 Akshay Bhopani
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished.
+
+// https://mit-license.org/
+
 import React, { useState, useEffect } from "react"
 import { TextField } from "@mui/material"
 import "./App.css"
